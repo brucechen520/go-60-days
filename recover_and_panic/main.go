20effbdba160d63215c0c37a0b6a32e9818bf0d8
brucechen520 <gibe329apt978@gmail.com>
2026-03-03 00:40:18 +0800

    add example inside the recover_and_panic

--- recover_and_panic/main.go
+++ recover_and_panic/main.go
@@ -2,6 +2,19 @@ package main
 
 // ref: https://go.cyub.vip/feature/panic-recover/
 
+// ref https://go.dev/blog/defer-panic-and-recover
+// Panic is a built-in function that stops the ordinary flow of control and begins panicking.
+// When the function F calls panic, execution of F stops, any deferred functions in F are executed normally,
+// and then F returns to its caller. To the caller, F then behaves like a call to panic.
+// The process continues up the stack until all functions in the current goroutine have returned, at which point the program crashes.
+// Panics can be initiated by invoking panic directly.
+// They can also be caused by runtime errors, such as out-of-bounds array accesses.
+
+// Recover is a built-in function that regains control of a panicking goroutine.
+// Recover is only useful inside deferred functions.
+// During normal execution, a call to recover will return nil and have no other effect.
+// If the current goroutine is panicking, a call to recover will capture the value given to panic and resume normal execution.
+
 import (
 	"fmt"
 	"sync"
@@ -14,17 +27,14 @@ func main() {
 
 	defer func() {
 		if r := recover(); r != nil {
-			fmt.Println("Recovered from panic in main:", r)
-		}
-	}()
-
-	defer func() {
-		if r := recover(); r != nil {
 			fmt.Println("Recovered from panic:", r)
 		}
 	}()
 
-	go case0(&wg) // 啟動一個 goroutine 執行 case
+	go case1(&wg) // 啟動一個 goroutine 執行 case
+
+	f() // ref https://go.dev/blog/defer-panic-and-recover
+	fmt.Println("Returned normally from f.")
 
 	wg.Wait() // 等待所有任務完成
 }
@@ -50,4 +60,25 @@ func case1(wg *sync.WaitGroup) {
 	}()
 
 	panic("Something went wrong!")
+}
+
+func f() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normally from g.")
+}
+
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v", i))
+	}
+	defer fmt.Println("Defer in g", i)
+	fmt.Println("Printing in g", i)
+	g(i + 1)
 }
